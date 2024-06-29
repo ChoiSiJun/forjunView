aa6c63e9c6c47e719e4a41de36cb2c73f93c1265
kjuny81 <kjuny81@mirtech.co.kr>
2024-06-29 22:47:27 +0900

    MirList 추가

---
+++ package/zmng/src/common/components/molecule/MirList.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function SelectedListItem() {
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleListItemClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    index: number,
+  ) => {
+    setSelectedIndex(index);
+  };
+
+  return (
+    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItemButton
+          selected={selectedIndex === 0}
+          onClick={(event) => handleListItemClick(event, 0)}
+        >
+          <ListItemText primary="Spam" secondary="July 20, 2014" />
+        </ListItemButton>
+      </List>
+    </Box>
+  );
+}
