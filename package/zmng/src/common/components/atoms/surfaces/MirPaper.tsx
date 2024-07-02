c6fda2356a539d824f582170e7b6ab609bf71190
kjuny81 <kjuny81@mirtech.co.kr>
2024-07-02 18:01:41 +0900

    07-02 수정

---
+++ package/zmng/src/common/components/atoms/surfaces/MirPaper.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material';
+
+const PaperStyled = styled(Paper)({});
+
+const MirPaper = ({}) => {
+    <Paper />
+};
+
+export default MirPaper;
---
+++ package/zmng/src/common/components/molecule/MirCard.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import MirCardContent from '@components/atoms/surfaces/MirCardContent';
+import MirPaper from '@components/atoms/surfaces/MirPaper';
+import { styled } from '@mui/material';
+
+interface MirButtonProps {
+  title: string;
+  onClick?: () => void;
+}
+
+const MirCard = ({}) => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default MirCard;
