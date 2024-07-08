23fe4d1c3bb9fded522cb04277ab3be168937ba1
sjchoi <sjchoi@mirtech.co.kr>
2024-07-08 17:54:41 +0900

    test

--- package/core/src/components/home/MirToolbarContent.tsx
+++ package/core/src/components/home/MirToolbarContent.tsx
@@ -1,18 +1,15 @@
-import { Box, Button, Toolbar, Typography } from '@mui/material';
+import { Toolbar, Typography } from '@mui/material';
 
 export interface MirToolbarContentProps {
   title: string;
 }
 
-const MirToolbarContent = ({ title = '툴바 제목' }: MirToolbarContentProps) => {
+const MirToolbarContent = ({ title }: MirToolbarContentProps) => {
   return (
     <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
       <Typography variant="h5" fontWeight="bold">
         {title}
       </Typography>
-      <Box sx={{ display: 'flex', marginLeft: 'auto', gap: '5px' }}>
-        <Button>테스트</Button>
-      </Box>
     </Toolbar>
   );
 };
