2c0aa4a87b58ca285ee9e56d6b7f9fbb8a9716bd
kjuny81 <kjuny81@mirtech.co.kr>
2024-07-11 23:56:13 +0900

    07-11 11:56 수정

--- package/zmng/src/module/system/components/LocationCreateModal.tsx
+++ package/zmng/src/module/system/components/LocationCreateModal.tsx
@@ -1,6 +1,7 @@
 import Grid from '@mui/material/Grid';
 import MirValidTextField from '@common/components/atoms/input/MirValidTextField';
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { useForm  } from "react-hook-form";
+import { useEffect } from 'react';
 import { useAppSelector, useAppDispatch } from '@config/ReduxHooks';
 import MirButton from '@common/components/atoms/button/MirButton';
 
@@ -11,13 +12,15 @@ import MirModalAction from '@common/components/atoms/modal/MirModalAction';
 
 import { createLocation } from '@module/system/slice/LocationSlice';
 import { modalClosed } from '@common/slice/ModalSlice';
-import { useSelector } from 'react-redux';
 
 export interface FormValues {
   "mloc": string;
   "name_ko": string;
   "zipcode": string;
   "address": string;
+  "address_detail": string,
+  "email": string,
+  "tel": string
 };
 
 export interface MirModalProps {
@@ -32,22 +35,26 @@ const LocationCreateModal = ({
   modalSize,
 }:MirModalProps) => {
 
-  const { handleSubmit, control } = useForm<FormValues> ({
+  const dispatch = useAppDispatch();
+  const modalOpen = useAppSelector((state) => state.Modal)
+
+  const { handleSubmit, control, reset } = useForm<FormValues> ({
     defaultValues: {
       mloc: "",
       name_ko: "",
       zipcode: "",
       address: "",
+      address_detail: "",
+      email: "",
+      tel: ""
     }
   });
 
-  const dispatch = useAppDispatch();
-  const modalOpen = useAppSelector((state) => state.Modal)
-
-  const onSubmit = (data: FormValues) => {
+  const createLocations = (data: FormValues) => {
     dispatch(createLocation({locationInfo: data, isOpen: false}))
       .then(() => {
         dispatch(modalClosed());
+        reset();
       })
   };
 
@@ -56,7 +63,7 @@ const LocationCreateModal = ({
       <MirModalTitle title={title} subTitle={subTitle} closeModal={() => dispatch(modalClosed())} />
 
       <MirModalContents>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(createLocations)}>
           <Grid container spacing={2}>
             <Grid item xs={6}>
               <MirValidTextField 
@@ -106,12 +113,54 @@ const LocationCreateModal = ({
                 }}
               />
             </Grid>
+            <Grid item xs={12}>
+              <MirValidTextField 
+                name="address_detail"
+                control={control}
+                // rules={{ required: "주소를 입력하세요." }}
+                textFieldProps={{
+                  label: "상세주소",
+                  id: "address_detail",
+                  placeholder: "상세주소를 입력하세요." 
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <MirValidTextField 
+                name="email"
+                control={control}
+                rules={{ 
+                  pattern: {
+                    value:
+                      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+                    message: "이메일 형식에 맞지 않습니다.",
+                  },
+                 }}
+                textFieldProps={{
+                  label: "이메일",
+                  id: "email",
+                  placeholder: "이메일을 입력하세요." 
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <MirValidTextField 
+                name="tel"
+                control={control}
+                // rules={{ required: "주소를 입력하세요." }}
+                textFieldProps={{
+                  label: "전화번호",
+                  id: "tel",
+                  placeholder: "전화번호를 입력하세요." 
+                }}
+              />
+            </Grid>
           </Grid>
         </form>
       </MirModalContents>
       
       <MirModalAction>
-        <MirButton ButtonType="default" buttonName="저장" onClick={handleSubmit(onSubmit)} />
+        <MirButton ButtonType="default" buttonName="저장" onClick={handleSubmit(createLocations)} />
         <MirButton ButtonType="default" buttonName="닫기" onClick={() => dispatch(modalClosed())} />
       </MirModalAction>
       
